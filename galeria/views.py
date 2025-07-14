5a04b0e5afcb086df9f119e2215ea201a5bae359
W4l14c3 <wallacedias2016@gmail.com>
2025-07-14 11:27:25 -0300

    Templates

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse #lib contem funções de resposta.
+#from django.http import HttpResponse lib contem funções de resposta.
 
 #Responsavel pela pagina principal.
 #Recebemos uma requisição.
 #Após preparar a view para exibir as informaçoes temos que fazer o roteamento.
+#Depois de configurar-mos o DIRS em TEMPLATES no setup,
+#todas as requisições de pagina irão renderizar automaticamente da pasta templates
+#return render(request, 'index.html')-> busca na pasta templates, indicado como diretorio base para templates.
+
 def index(request):
-    return HttpResponse('<h1>Aluar Space<h1>')
+    return render(request, 'index.html')
