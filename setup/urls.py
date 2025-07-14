81ea01db211f3d93bd67a88c10bac47b13aeb547
W4l14c3 <wallacedias2016@gmail.com>
2025-07-14 09:59:22 -0300

    Urls

--- setup/urls.py
+++ setup/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from galeria.views import index
 
 #Aqui é onde fazemos o roteamento das aplicações.
-
+#Quando ocorre uma requisição ela é recebida pela urls.py que procura o padrão correspondente.
+#Que ira devolver o conteudo, mas para isso acontecer temos que indicar a rota.
+#Como esta: path('', index) indica que quando etiver-mos na pagina raiz.
+#Execute a função index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', index),
+    path('', index),
 ]
