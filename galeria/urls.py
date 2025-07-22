a5d295ad939161a681956ea2a5773be2faa4d997
W4l14c3 <wallacedias2016@gmail.com>
2025-07-22 17:11:19 -0300

    URL name

--- galeria/urls.py
+++ galeria/urls.py
@@ -5,10 +5,14 @@ from galeria.views import index, imagem
 Cada aplicação será responsavel por suas rotear suas urls.
 Aqui somente indicamos as urls da aplicação galeria importando suas funções.
 Este arquivo foi incluido no Setup/urls.py(Herança)
+
+Agora o Django encontra o caminho para executar as funções da views, através do name, 
+pois os links estão com codigo embedado python.
+
 '''
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 
 ]
