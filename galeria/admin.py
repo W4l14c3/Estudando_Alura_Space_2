7aef0ab176f85916446b9e882369905462f5bd96
W4l14c3 <wallacedias2016@gmail.com>
2025-08-19 11:21:57 -0300

    06CrudNoAdmin - 07IncluindoCategoria

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+#Permite a personalização da pagina admin.
+class ListandoFotografias(admin.ModelAdmin):
+
+    list_display = ("id", "nome", "legenda")#Cria uma lista de exibição.
+    list_display_links = ("id", "nome")#Torna alguns campos lincados.
+    search_fields = ("nome",)#Adiciona um campo de busca.
+
+
+#Toda nova classe criada deve ser adicionada no register para aplicar as mudanças.
+admin.site.register(Fotografia, ListandoFotografias)
